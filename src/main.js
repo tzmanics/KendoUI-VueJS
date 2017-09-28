4c4db1c7563bab14df1b2e02b273b0af6ebe9cca
tzmanics <tzmanics@gmail.com>
2017-09-27 23:59:04 -0400

    install kendo components and add to main.js

--- src/main.js
+++ src/main.js
@@ -4,12 +4,30 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import '@progress/kendo-ui'
+import {
+  KendoGrid,
+  KendoGridColumn,
+  KendoGridInstaller
+} from '@progress/kendo-grid-vue-wrapper'
+import {
+  KendoDataSource,
+  KendoDataSourceInstaller
+} from '@progress/kendo-datasource-vue-wrapper'
+
 Vue.config.productionTip = false
+Vue.use(KendoGridInstaller)
+Vue.use(KendoDataSourceInstaller)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {
+    App,
+    KendoGrid,
+    KendoGridColumn,
+    KendoDataSource
+  }
 })
