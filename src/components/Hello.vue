4964fc7852f7d20c3d0c5ed8fd8002ae35a249c9
tzmanics <tzmanics@gmail.com>
2017-10-04 23:45:15 -0400

    add kendo ui grid component

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,50 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="grid">
+    <kendo-datasource
+      ref="datasource"
+      :type="'odata'"
+      :pageSize="20"
+      :transportRead="'https://demos.telerik.com/kendo-ui/service/Northwind.svc/Customers'"
+    ></kendo-datasource>
+    <kendo-grid
+      :height="550"
+      :dataSourceRef="'datasource'"
+      :groupable='true'
+      :sortable='true'
+      :pageable-refresh='true'
+      :pageable-page-sizes='true'
+      :pageable-button-count="5">
+      <kendo-grid-column
+        field="ContactName"
+        title="Contact Name"
+        :width="250">
+      </kendo-grid-column>
+      <kendo-grid-column
+        field="ContactTitle"
+        title="Contact Title">
+      </kendo-grid-column>
+      <kendo-grid-column
+        field="CompanyName"
+        title="Company Name">
+      </kendo-grid-column>
+      <kendo-grid-column
+        field="Country"
+        title="Country"
+        :width="150">
+      </kendo-grid-column>
+    </kendo-grid>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  name: 'grid'
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  .grid {
+    padding: 10px;
+  }
 </style>
